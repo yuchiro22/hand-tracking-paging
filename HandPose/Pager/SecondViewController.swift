9540f75a32746f06e06fa84fc2270bc2361a672b
yuchiro22 <t.yuyu.alies@gmail.com>
2020-09-29 01:36:24 +0900

    create base pageViewController

---
+++ HandPose/Pager/SecondViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SecondViewController.swift
+//  HandPose
+//
+//  Created by Yuichiro Tsuji on 2020/09/28.
+//  Copyright © 2020 Apple. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
---
+++ HandPose/Pager/ThirdViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ThirdViewController.swift
+//  HandPose
+//
+//  Created by Yuichiro Tsuji on 2020/09/28.
+//  Copyright © 2020 Apple. All rights reserved.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
